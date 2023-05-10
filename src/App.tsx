b8724a63767af0f22cd618ea501f0cbb0e0337e9
lai2-01 <lai2-01@itismeucci.com>
2023-05-10 11:46:31 +0200

    Test Table for React

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect} from 'react';
+
+const data = [
+  { name: "Anom", age: 19, gender: "Male" },
+  { name: "Megha", age: 19, gender: "Female" },
+  { name: "Subham", age: 25, gender: "Male"},
+]
+
+const form = false;
 
 function App() {
+  const [students, setStudents] = useState(data);
+  const [showForm, setShowForm] = useState(form);
+  const [_name, setName] = useState('');
+  const [_age, setAge] = useState('');
+  const [_gender, setGender] = useState('');
+
+  function delete_(el: String){
+    setStudents(students.filter(obj => obj.name !== el));
+  }
+
+  function add(){
+    students.push({name: _name, age: parseInt(_age), gender: _gender});
+    setStudents(students);
+    setShowForm(false);
+  }
+
+  function show_set() {
+      if (showForm) {
+        setShowForm(false);
+      }else{
+        setShowForm(true);
+      }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <h1>Test Tabella</h1>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+            <th>Delete</th>
+          </tr>
+          {students.map((val, key) => {
+            return (
+              <tr key={key}>
+                <td>{val.name}</td>
+                <td>{val.age}</td>
+                <td>{val.gender}</td>
+                <td><button onClick={() => delete_(val.name)}>Elimina</button></td>
+              </tr>
+            )
+          })}
+        </table>
+        <div>
+          <button onClick={()=> show_set()} className="add" >Inserisci</button>
+        </div>
+        { showForm &&
+          <form method='post' onSubmit={() => add()}>
+            <div>
+              <label htmlFor="Name">Name</label>
+            </div>
+            <div>
+              <input type="text" id='Name' onChange={(name) => setName(name.target.value)} required/>
+            </div>
+            <div>
+              <label htmlFor="Age" >Age</label>
+            </div>
+            <div>
+              <input type="number" id='Age' onChange={(age) => setAge(age.target.value)} required/>
+            </div>
+            <div>
+            <label htmlFor="Gender">Gender</label>
+            </div>
+            <div>
+              <select id='Gender' onChange={(gender) => setGender(gender.target.value)} required>
+                <option value="Female"> Female </option>
+                <option value="Male"> Male </option>
+              </select>
+            </div>
+            <div>
+              <input type="submit" value={"Conferma"} className="submit_btn"/>
+            </div>
+          </form>
+        }
+      </center>
     </div>
   );
 }
